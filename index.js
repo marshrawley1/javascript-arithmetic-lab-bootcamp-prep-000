d0cc534d135a3dae98b0bc3824ff077012aedce4
marshrawley1 <marshallrawley@gmail.com>
2017-11-10 16:24:08 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,18 +14,18 @@ function divide(a,b) {
 return  a/b
 }
 
-function inc(a,b) {
+function inc(n) {
 return  n++
 }
 
-function dec(a,b) {
+function dec(n) {
 return  n--
 }
 
 function makeInt(n){
-  parseInt(n,10)
+return  parseInt(n,10)
 }
 
 function preserveDecimal(n) {
-  parseFloat(n)
+return  parseFloat(n)
 }
